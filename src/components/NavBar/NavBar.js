5528e5790af8942423b4bb448ea48aa31a97e51d
gjaic2016 <gjaic23@gmail.com>
2020-12-28 16:42:50 +0100

    navbar buttons

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { Button } from 'react-bootstrap';
-
+import { Button } from "react-bootstrap";
 
 const NavBar = () => {
   return (
     <>
-      
-      <div>
-        <Button variant="primary"  >Početna</Button>
-        <Button variant="primary"  >Oglasi</Button>
-        <Button variant="primary"  >O nama</Button>
-        <Button variant="primary"  >Registracija</Button>
-        <Button variant="primary"  >Prijava</Button>
+      <div className="navbar_wrapper" >
+        <div >
+          <Button variant="primary">Početna</Button>
+          <Button variant="primary">Oglasi</Button>
+          <Button variant="primary">O nama</Button>
+          <Button variant="primary">Registracija</Button>
+          <Button variant="primary">Prijava</Button>
+        </div>
       </div>
     </>
   );
