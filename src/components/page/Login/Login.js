f54414a23080aceb3ac51d08ae8a1cde2d273169
gjaic2016 <gjaic23@gmail.com>
2021-01-03 18:59:30 +0100

    login response test

--- src/components/page/Login/Login.js
+++ src/components/page/Login/Login.js
@@ -1,15 +1,36 @@
 import React from "react";
+import Axios from "axios";
 import { useState } from "react";
-import { Button, Card, Form, FormControl, InputGroup } from "react-bootstrap";
+import { Button, Card, Form } from "react-bootstrap";
+// import Toast from "react-bootstrap/Toast";
+// import { Link } from "react-router-dom";
 
 const Login = () => {
-
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  const [loginStatus, setLoginStatus] = useState("");
+
+  // const [show, setShow] = useState(false);
+  // const toggleShow = () => setShow(!show);
+
+  const login = () => {
+    Axios.post("http://localhost:3001/login", {
+      username: username,
+      password: password,
+    }).then((response) => {
+      if (response.data.message) {
+        setLoginStatus(response.data.message);
+      } else {
+        setLoginStatus(response.data[0].username);
+      }
+    });
+  };
 
   return (
     <>
       <div className="container">
+         <div>{loginStatus}</div>
         <Card className="card-styling-log card-size shadow">
           <Card.Header>
             <h2>Prijava</h2>
@@ -35,9 +56,20 @@ const Login = () => {
                 </Form.Group>
               </Form>
             </Card.Text>
-            <Button variant="primary">Prijava</Button>
+            <Button variant="primary" onClick={login}>
+              Prijava
+            </Button>
           </Card.Body>
         </Card>
+        {/* <div>
+          <Toast className="toast" onClose={toggleShow} closeButton animation={false}>
+            <Toast.Header></Toast.Header>
+            <Toast.Body>
+              <h3>{loginStatus}</h3>
+            </Toast.Body>
+          </Toast>
+        </div> */}
+       
       </div>
     </>
   );
